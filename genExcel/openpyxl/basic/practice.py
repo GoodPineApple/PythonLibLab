771eb306caf4c3c946eef1dfdc26832ffa5dc9de
Taemin Kim <taemin3000@naver.com>
2017-08-08 10:09:43 +0900

    make example.xlsx with openpyxl

--- genExcel/openpyxl/basic/practice.py
+++ genExcel/openpyxl/basic/practice.py
@@ -13,3 +13,30 @@ ws1.title = "Example1"
 
 # 1. 시트의 행렬의 번호를 입력하여 Text 입력이 가능
 ws1['A1'] = "A1_Test Text"
+
+# 2-1 시트에서 셀을 변수로 만들고
+c = ws1.cell('A2')
+
+# 2-2 아래처럼 각 셀에 대해 value 입력, font 설정 등을 할 수 있다.
+c.value = "A2 Zone"
+c.font = Font(name='Arial', size=14)
+c.border = Border(left=Side(border_style="thin", color='FF000000'),
+                  right=Side(border_style="thin", color='FF000000'),
+                  top=Side(border_style="thin",color='FF000000'),
+                  bottom=Side(border_style="thin", color='FF000000'))
+
+# 3. row, colum 을 지정하여 셀을 변수로 리턴할 수 있다.
+d = ws1.cell(row=2,column=2)
+d.value = "(2,2)text"
+
+# 같은 workbook에 새로운 시트를 추가할 때
+ws2 = wb.create_sheet()
+
+# 번호를 지정하면 아래 번호에 sheet를 추가한다.
+# ws2 = wb.create_sheet(1)
+
+# 새로운 시트의 이름은 Sheet2라고 한다.
+ws2.title = "Sheet2"
+
+# 저장
+wb.save('example.xlsx')
