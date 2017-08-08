4e287ba5640eb815595b98d60f1a21ba9a22fd40
Taemin Kim <taemin3000@naver.com>
2017-08-08 10:00:51 +0900

    practice openpyxl basic grammer

--- genExcel/openpyxl/basic/practice.py
+++ genExcel/openpyxl/basic/practice.py
@@ -4,3 +4,12 @@
 from openpyxl import Workbook
 from openpyxl.styles import Font, Side, Border
 
+# Workbook 생성
+wb = Workbook()
+
+# Workbook을 생성하면 적어도 하나의 워크시트를 생성한다.
+ws1 = wb.active
+ws1.title = "Example1"
+
+# 1. 시트의 행렬의 번호를 입력하여 Text 입력이 가능
+ws1['A1'] = "A1_Test Text"
