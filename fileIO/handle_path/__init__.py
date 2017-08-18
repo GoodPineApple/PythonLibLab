b84ba1c9f471e36be51c370819c76a00f16b3393
Taemin Kim <taemin3000@naver.com>
2017-08-18 12:23:47 +0900

    config data path

--- fileIO/handle_path/__init__.py
+++ fileIO/handle_path/__init__.py
@@ -1,13 +1,13 @@
-# 출처: http://itissmart.tistory.com/49 [IT is Smart]
+# http://harryp.tistory.com/154
 
 # read 참고
-fw = open('../datas/sample.txt', 'w')
+fw = open('/../datas/sample.txt', 'w')
 fw.write('Writing some stuff in my text file\n')
 fw.write('I like bacon\n')
 fw.close()
 
 # 'r'은 파일 내의 내용을 읽어온다.
-fr = open('../datas/sample.txt', 'r')
+fr = open('/../datas/sample.txt', 'r')
 text = fr.read()
 print(text)
 fr.close()
